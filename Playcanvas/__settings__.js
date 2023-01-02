13483de2d2f723534fd645b1ed1577eb38a39039
ErikSeewald <erik.seewaldx@gmail.com>
2023-01-02 16:47:24 +0100

    added 3 types of hit interactions

---
+++ Playcanvas/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1588876.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 114904953, 114904954, 114909942, 114918682, 115129333, 115638044 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/116694325/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/116694326/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/116694324/1/ammo.js', 'preload' : true},
+];
